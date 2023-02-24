76644ef9a573d87a23aed35bbe5961f7de9eca1c
Artyel <artyel@icloud.com>
2023-02-24 13:16:14 +0000

    Formated code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-    int ticketPrice = 50;
-    int ratio = 20;
-        System.out.println(( ticketPrice/ratio) + " mile (s)");
+        int ticketPrice = 50;
+        int ratio = 20;
+        System.out.println((ticketPrice / ratio) + " mile (s)");
     }
 }
